cf1bfe7d7b25ea9d826fca80a24f16f8f29e76b3
ZekReshi <florian.schwarcz@gmx.at>
2018-04-26 11:44:33 +0200

    Created pattern

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -2,7 +2,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
-    public static Matcher getMatcher(String dateString) {
-        return null;
+    private static Pattern pattern = Pattern.compile("^([012]?\\d|[3][01])[ .-]([0]?\\d|[1][012])[ .-]((19|20)?(\\d))?\\d$");
+
+    public static Matcher getMatcher(String dateString){
+        return pattern.matcher(dateString);
     }
 }
